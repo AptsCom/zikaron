5253c53d4f7d125a6f2202a2caf18bb223929c76
Jay Wengrow <jaywngrw@gmail.com>
2013-07-16 13:56:57 -0500

    should not raise error trying to flush empty cache

--- lib/zikaron.rb
+++ lib/zikaron.rb
@@ -18,7 +18,7 @@ module Zikaron
 
   def self.flush_cache
     return unless redis_exists?
-    redis.del(redis.keys("zikaron*"))
+    redis.del(redis.keys("zikaron*")) unless redis.keys("zikaron*").empty?
   end
 
   def self.redis
